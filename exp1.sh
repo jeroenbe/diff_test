114707512db2f14c6861dfc9899529858e945e24
jeroen berrevoets <jeroen.berrevoets@gmail.com>
2023-10-04 20:58:16 +0200

    add shell script for experiments

---
+++ exp1.sh
@@ -0,0 +1,32 @@
+python -m run --run-count 10 -d 20 -n 1000 \
+ -m notears \
+ -m notears-np \
+ --group exp1
+
+
+ python -m run --run-count 10 -d 10 -n 1000 \
+ -m notears \
+ -m notears-np \
+ --group exp1
+
+ python -m run --run-count 10 -d 30 -n 1000 \
+ -m notears \
+ -m notears-np \
+ --group exp1
+
+
+ python -m run --run-count 10 -d 20 -n 1000 --sem-type mlp\
+ -m notears \
+ -m notears-np \
+ --group exp1
+
+ python -m run --run-count 10 -d 20 -n 1000 --sem-type mlp\
+ -m notears \
+ -m notears-np \
+ --group exp1
+
+
+ python -m run --run-count 10 -d 30 -n 1000 --sem-type mlp\
+ -m notears \
+ -m notears-np \
+ --group exp1
