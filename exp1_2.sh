114284c45cc30db43ccc376ca25526d9abdb5fc7
jeroen berrevoets <jeroen.berrevoets@gmail.com>
2023-10-05 19:43:24 +0200

    new exp

---
+++ exp1_2.sh
@@ -0,0 +1,38 @@
+python -m run --run-count 10 -d 20 -n 1000 \
+ -m dagma \
+ -m ges \
+ -m dagma-np \
+ --group exp1
+
+
+ python -m run --run-count 10 -d 10 -n 1000 \
+-m dagma \
+ -m ges \
+ -m dagma-np \
+ --group exp1
+
+ python -m run --run-count 10 -d 30 -n 1000 \
+-m dagma \
+ -m ges \
+ -m dagma-np \
+ --group exp1
+
+
+ python -m run --run-count 10 -d 20 -n 1000 --sem-type mlp\
+-m dagma \
+ -m ges \
+ -m dagma-np \
+ --group exp1
+
+ python -m run --run-count 10 -d 20 -n 1000 --sem-type mlp\
+-m dagma \
+ -m ges \
+ -m dagma-np \
+ --group exp1
+
+
+ python -m run --run-count 10 -d 30 -n 1000 --sem-type mlp\
+-m dagma \
+ -m ges \
+ -m dagma-np \
+ --group exp1
