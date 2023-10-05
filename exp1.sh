597bc68f9c17caa8ea8c16cade4d81da452bb83f
jeroen berrevoets <jeroen.berrevoets@gmail.com>
2023-10-05 22:09:26 +0200

    continue exp

--- exp1.sh
+++ exp1.sh
@@ -1,16 +1,16 @@
-python -m run --run-count 10 -d 20 -n 1000 \
- -m notears \
- -m notears-np \
- --group exp1
+# python -m run --run-count 10 -d 20 -n 1000 \
+#  -m notears \
+#  -m notears-np \
+#  --group exp1
 
 
- python -m run --run-count 10 -d 10 -n 1000 \
- -m notears \
- -m notears-np \
- --group exp1
+#  python -m run --run-count 10 -d 10 -n 1000 \
+#  -m notears \
+#  -m notears-np \
+#  --group exp1
 
  python -m run --run-count 10 -d 30 -n 1000 \
- -m notears \
+ #-m notears \
  -m notears-np \
  --group exp1
 
